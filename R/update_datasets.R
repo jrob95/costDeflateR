a7740aee82727d736d0d138685066afd7923844a
Jack Roberts <93294554+jrob95@users.noreply.github.com>
2025-07-30 15:39:34 +1000

    New workflow for dev side internal data updates

--- R/update_datasets.R
+++ R/update_datasets.R
@@ -1,20 +1,49 @@
-# Script for development team to update internal (redundancy) data sets.
+#' Update internal data from IMF and OECD sources
+#'
+#' @description
+#' This internal function is intended for package developers only. It refreshes
+#' the internal datasets used for deflation and PPP conversion by pulling the
+#' latest values from IMF and OECD SDMX endpoints.
+#'
+#' @details
+#' This function is not exported and should not be called by end users. It is
+#' used during package development to ensure that internal datasets are current.
+#' The function also stores a timestamp of the last update for provenance.
+#'
+#' @keywords internal
+#' @usage NULL
+#' @return Invisibly returns a list of updated datasets
+#' @noRd
+dev_update_data <- function() {
+  message("Updating internal data from IMF and OECD...")
 
+  # 1. Fetch data
+  oecd_ppp <- get_oecd_ppp()
+  imf_ppp  <- get_imf_ppp()
+  imf_gdpd <- get_imf_gdpd()
 
+  # 2. Save datasets
+  usethis::use_data(oecd_ppp, overwrite = TRUE)
+  usethis::use_data(imf_ppp,  overwrite = TRUE)
+  usethis::use_data(imf_gdpd, overwrite = TRUE)
 
-############
-# OECD PPP #
-############
+  # 3. Save timestamp
+  update_meta <- list(
+    updated_at = Sys.time(),
+    updated_by = Sys.info()[["user"]],
+    sources = list(
+      oecd_ppp_key = "PPP_B1GQ",
+      imf_ppp_key  = "*.PPPGDP.*",
+      imf_gdpd_key = "*.NGDP_D.*"
+    )
+  )
+  usethis::use_data(update_meta, overwrite = TRUE)
 
-
-oecd_url_stub <- "https://sdmx.oecd.org/public/rest/"
-oecd_dataset <- "data/OECD.SDD.NAD,DSD_NAMAIN10@DF_TABLE4,2.0/"
-oecd_filter <- "A.AUS+AUT+BEL+CAN+CHL+COL+CRI+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+JPN+KOR+LVA+LTU+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA...PPP_B1GQ.......?dimensionAtObservation=AllDimensions"
-# as far as I can tell, "Purchasing power parities for GDP" (PPP_B1GQ) were used, not "Purchasing Power Parities for actual individual consumption" (PPP_P41)
-# see https://gitlab.algobank.oecd.org/public-documentation/dotstat-migration/-/raw/main/OECD_Data_API_documentation.pdf for more information
-
-oecd_url <- paste0(oecd_url_stub,oecd_dataset, oecd_filter)
-
-### Developer -> update internal data sets
-oecd <- parse_oecd_json(oecd_url)
-usethis::use_data(oecd_ppp, overwrite = TRUE)
+  invisible(list(
+    oecd_ppp = oecd_ppp,
+    imf_ppp  = imf_ppp,
+    imf_gdpd = imf_gdpd,
+    update_meta = update_meta
+  ))
+}
+dev_update_data()
