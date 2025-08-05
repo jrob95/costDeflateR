f47a34c70f3c88c09643970d59ed358b6885fa25
Jack Roberts <93294554+jrob95@users.noreply.github.com>
2025-08-05 13:49:25 +1000

    refactor deflate

--- R/costdeflate.R
+++ R/costdeflate.R
@@ -10,13 +10,16 @@
 #' year on each row, or applying a pre specified country/ year for each row.
 #'
 #' @param input_data A `data.frame`.
-#' @param country_base A `character` the name of a country OR the column name representing the base country.
-#' @param year_base A numeric or column name representing the base year.
-#' @param year_target A numeric or column name representing the target year.
-#' @param country_target A character or column name representing the target country.
-#' @param cost_base A numeric or column name representing the base cost.
-#' @param cost_target String, what should the name be for the new column with deflated cost values.
-#' @param rename_countries A Logical
+#' @param country_base A `character` the name of a country OR the `colname` representing the base country.
+#' @param year_base A `numeric` or `colname` representing the base year.
+#' @param year_target A `numeric` or `colname` representing the target year.
+#' @param country_target A `character` or `colname` representing the target country.
+#' @param cost_base A `numeric` or `colname` representing the base cost.
+#' @param cost_target A `character`, what should the name be for the new column with deflated cost values.
+#' @param rename_countries A `logical`, ask the function to clean up country names if not in `iso3c` format.
+#' @param pppex_src A `character`, which dataset should PPP values come from? IMF or OECD?
+#' @param use_live_data A `logical`. Makes call to `update_internal_data()`, if current IMF or OECD are more than a week old then make an API call to replace them., default = `TRUE`
+#' @param force_live_data A `logical`. Makes call to `update_internal_data()` regardless of age of data currently stored. Use only if you know IMF WEO or OECD data has been updated since you last ran `deflate`, default = `FALSE`
 #'  Default to \code{"cost_target"}
 #'
 #' @return Returns a dataframe with deflated cost values.
@@ -53,41 +56,55 @@
 #'   year_base = "2010",
 #'   country_base = "Australia",
 #'   year_target = "2020",
-#'   country_target = "United States"
+#'   country_target = "United States",
+#'   rename_countries = TRUE
 #' )
 #'
 #' @export
-deflate <- function(input_data = .x, cost_base, year_base, country_base, year_target, country_target, cost_target = "cost_target", rename_countries = FALSE, use_live_data = TRUE) {
-
-  # get live data, store temp.
-  if (use_live_data == TRUE) {
-    tryCatch(
-      {
-        update_internal_data()
-        packageStartupMessage(
-          "Using latest IMF & OECD datasets - updated ",
-          Sys.time()
-        )
-      },
-      error = function(e) {
-        last <- tryCatch(
-          get("update_meta",
-            envir = asNamespace("costDeflateR")
-          )[["updated_at"]],
-          error = function(e) "unknown"
-        )
-        warning(
-          "Failed to prepare temporary data on load: ",
-          conditionMessage(e),
-          "\n Reverting to internal data. Last updated: ",
-          last
-        )
-      }
-    )
-  }
-
+deflate <- function(input_data = .x, cost_base, year_base, country_base, year_target, country_target, cost_target = "cost_target", pppex_src = "IMF", rename_countries = FALSE, use_live_data = TRUE, force_live_data = FALSE) {
   tryCatch(
     {
+      if (!pppex_src %in% c("IMF", "OECD")) {
+        stop(paste0("`pppex_src` must be either \"IMF\" or \"OECD\"\"", pppex_src, "\" not allowed. Check capitals/ case."))
+      }
+
+      # deal with internal/ live data.
+      dl_imfppp <- dl_oecdppp <- FALSE
+
+      if (pppex_src == "IMF") dl_imfppp <- TRUE
+      if (pppex_src == "OECD") dl_oecdppp <- TRUE
+      # get live data, store temp.
+      if (use_live_data) {
+        message("Attempting to use live data from IMF/ OECD")
+        cond_update_internal_data(force_live_data, dl_imfppp, dl_oecdppp)
+        dir <- get_temp_data_dir()
+        oecd_ppp <- readRDS(file.path(dir, "oecd_ppp.rds"))
+        imf_ppp <- readRDS(file.path(dir, "imf_ppp.rds"))
+        imf_gdpd <- readRDS(file.path(dir, "imf_gdpd.rds"))
+      } else {
+        message(paste0("Using internal data. Last upated", update_meta[updated_at]))
+      }
+
+      # point data to correct dataset.
+      if (pppex_src == "IMF") {
+        ppp_vals <- imf_ppp |>
+          dplyr::select(country = COUNTRY, year = TIME_PERIOD, value = PPPEX)
+      }
+      if (pppex_src == "OECD") {
+        ppp_vals <- oecd_ppp |>
+          dplyr::select(country = COUNTRY, year = TIME_PERIOD, value = PPP)
+      }
+
+      if (TRUE) {
+        gdpd_vals <- imf_gdpd |>
+          dplyr::select(country = COUNTRY, year = TIME_PERIOD, value = NGDP_D)
+      }
+
+      # country combs for later
+      tbl <- ppp_vals |>
+        dplyr::inner_join(gdpd_vals, dplyr::join_by(country, year), suffix = c("_pppex", "_gdpd"))
+
+      # check parameters####
       # handler to throw errors if desired object is not class
       if (!is.data.frame(input_data) & !tibble::is_tibble(input_data)) {
         stop("Data must be in the format of a `data.frame` or `tibble`")
@@ -126,7 +143,7 @@ deflate <- function(input_data = .x, cost_base, year_base, country_base, year_ta
           num <- as.numeric(x) # Convert to numeric
           return(num)
         } else {
-          stop("All elements must be numeric, or characters with only numbers")
+          stop(sprintf("Field '%s' must be numeric or contain only numeric characters.", field_name))
         }
       }
 
@@ -136,97 +153,47 @@ deflate <- function(input_data = .x, cost_base, year_base, country_base, year_ta
 
 
       # Check if country_base is included in gdpd_vals or is a column in data frame
-      # Fuzzy match with agrep if name is close
-      check_countries <- function(country_vector) {
-        if (!is.character(country_vector)) {
-          stop("Country vector must be a character")
-        }
-
-        # Convert country_vector to title case and create a factor
-        country_vector <- tools::toTitleCase(country_vector)
-
-        # Get unique values from country_vector
-        unique_countries <- unique(country_vector)
-        unique_in_reference <- unique(tools::toTitleCase(gdpd_vals$Country))
-
-        # Initialize an empty vector to store replacements
-        replacements <- country_vector
-
-        # Initialize empty vectors to store indices of matched countries
-        matched_indices_country_vector <- integer(0)
-        matched_indices_gdpd_vals <- integer(0)
-
-        for (country in unique_countries) {
-          # Skip the value if it exactly matches any value in unique_in_reference
-          if (country %in% unique_in_reference) {
-            next # Skip to the next country
-          }
-
-          # Fuzzy match country with unique_in_reference
-          matched_index <- agrep(country, unique_in_reference, max.distance = 0.1) # Adjust max.distance as needed
-
-          # If a match is found, replace and print the replacement
-          if (length(matched_index) > 0) {
-            replacement <- unique_in_reference[matched_index[1]] # Take the first match
-            matched_indices_country_vector <- c(matched_indices_country_vector, which(country_vector == country)) # Add indices of matched countries in country_vector
-            matched_indices_gdpd_vals <- c(matched_indices_gdpd_vals, matched_index) # Add indices of matched countries in unique_in_reference
-            cat("Country checker function replaced", country, "with", replacement, "\n")
-          }
-        }
-
-        # Replace values in country_vector with replacements for matched indices
-        replacements[matched_indices_country_vector] <- unique_in_reference[matched_indices_gdpd_vals]
-
-        # # Update country_vector with replacements
-        country_vector <- replacements
-
-        # check that is a good match
-        countries_in_gdpd <- country_vector %in% gdpd_vals$Country
-        if (!all(countries_in_gdpd)) {
-          invalid_countries <- country_vector[!countries_in_gdpd]
-          stop(writeLines(paste0("\`", invalid_countries, "\` or a similar name is not included in the WEO reference table. Use deflator_country_list() for a list of acceptable countries/ country names")))
-        }
-
-        return(country_vector)
+      if (rename_countries) {
+        input_data[["country_base2"]] <- country_cleaner(input_data[[country_base]])
+        input_data[["country_target2"]] <- country_cleaner(input_data[[country_target]])
+      } else {
+        input_data[["country_base2"]] <- input_data[[country_base]]
+        input_data[["country_target2"]] <- input_data[[country_target]]
       }
 
-
-      input_data[[country_base]] <- check_countries(input_data[[country_base]])
-      input_data[[country_target]] <- check_countries(input_data[[country_target]])
-
-
       # # Check if the combination of year and country is available in the data
-      check_year_country_combination <- function(x, year_field, country_field) {
+      check_year_country_combination <- function(x, year_field, country_field, tbl) {
         # get number of unique combinations'
         combs <- unique(paste0(x[[country_field]], " - ", as.character(x[[year_field]])))
-        ref_comb <- paste0(gdpd_vals$Country, " - ", as.character(gdpd_vals$year))
+        ref_comb <- paste0(tbl$country, " - ", as.character(tbl$year))
         # check whether the combinations are in the reference table
         for (comb in combs) {
           if (!(comb %in% ref_comb)) {
-            stop(paste0("\"", comb, "\" does not exist in the reference table. Please use delfator_country_year_combs() to check available combinations"))
+            # stop(paste0("\"", comb, "\" does not exist in the reference table. Please use `delfator_country_year_combs()` to check available combinations"))
+            stop(paste0("\"", comb, "\" does not exist in the reference table. Please check available combinations"))
           }
         }
       }
 
-      check_year_country_combination(input_data, year_base, country_base)
-      check_year_country_combination(input_data, year_target, country_target)
+      check_year_country_combination(input_data, year_base, "country_base2", tbl)
+      check_year_country_combination(input_data, year_target, "country_target2", tbl)
 
       # deflate the cleaned input data
       output_data <- input_data %>%
-        dplyr::left_join(gdpd_vals, dplyr::join_by(!!country_base == "Country", !!year_base == "year")) %>%
-        dplyr::mutate(deflate_orig = as.numeric(value)) %>%
-        dplyr::select(-value) %>%
-        dplyr::left_join(gdpd_vals, dplyr::join_by(!!country_target == "Country", !!year_target == "year")) %>%
-        dplyr::mutate(deflate_target = as.numeric(value)) %>%
-        dplyr::select(-value) %>%
-        dplyr::left_join(ppp_vals, dplyr::join_by(!!country_base == "Country", !!year_base == "year")) %>%
-        dplyr::mutate(PPP_orig = as.numeric(value)) %>%
-        dplyr::select(-value) %>%
-        dplyr::left_join(ppp_vals, dplyr::join_by(!!country_target == "Country", !!year_target == "year")) %>%
-        dplyr::mutate(PPP_target = as.numeric(value)) %>%
-        dplyr::select(-value) %>%
+        dplyr::left_join(tbl, dplyr::join_by("country_base2" == "country", !!year_base == "year")) %>%
+        dplyr::mutate(
+          deflate_orig = as.numeric(value_gdpd),
+          PPP_orig = as.numeric(value_pppex)
+        ) %>%
+        dplyr::select(-value_gdpd, -value_pppex) %>%
+        dplyr::left_join(tbl, dplyr::join_by("country_target2" == "country", !!year_target == "year")) %>%
+        dplyr::mutate(
+          deflate_target = as.numeric(value_gdpd),
+          PPP_target = as.numeric(value_pppex)
+        ) %>%
+        dplyr::select(-value_gdpd, -value_pppex) %>%
         dplyr::mutate(cost_target = (deflate_target * PPP_target) / (deflate_orig * PPP_orig) * .data[[cost_base]]) %>%
-        dplyr::select(-c("PPP_orig", "deflate_orig", "PPP_target", "deflate_target"))
+        dplyr::select(-c("PPP_orig", "deflate_orig", "PPP_target", "deflate_target", "country_base2", "country_target2"))
       return(output_data)
     },
     error = function(e) {
@@ -254,7 +221,36 @@ deflator_country_list <- function() {
 #' This is a helper function for checking which countr/ year combinations  exist in the reference data
 #'
 #' @return Returns a \code{chr}.
-#' @export
 delfator_country_year_combs <- function() {
   return(paste0(gdpd_vals$Country, " - ", as.character(gdpd_vals$year)))
+}
+
+use_live_data <- function() {
+  tryCatch(
+    {
+      update_internal_data()
+      packageStartupMessage(
+        "Using latest IMF & OECD datasets - updated ",
+        Sys.time()
+      )
+    },
+    error = function(e) {
+      last <- tryCatch(
+        get("update_meta",
+          envir = asNamespace("costDeflateR")
+        )[["updated_at"]],
+        error = function(e) "unknown"
+      )
+      warning(
+        "Failed to prepare temporary data on load: ",
+        conditionMessage(e),
+        "\n Reverting to internal data. Last updated: ",
+        last
+      )
+    }
+  )
+}
+
+country_cleaner <- function(data) {
+  countrycode::countryname(data, destination = "iso3c")
 }
