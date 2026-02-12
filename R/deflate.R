eeeb913a2b2d047eee58e5af354b7c42d0ee8d2e
Rex Parsons <47549264+RWParsons@users.noreply.github.com>
2026-02-12 16:04:27 +1000

    adjust checks for delate() and clean up coding style

--- R/deflate.R
+++ R/deflate.R
@@ -8,15 +8,15 @@
 #' You can either specify a base and target country/year for each row via column names,
 #' or provide constant values to apply across all rows.
 #'
-#' @param input_data A `data.frame` containing the data to be deflated.
-#' @param country_base A `character` string: either a country name or the name of a column in `input_data` containing base countries.
+#' @param input_data A `data.frame` containing the data to be deflated. Can be missing if values (or vectors of values) are specified for the cost, year and country base and targets.
+#' @param cost_base A `character`: the name of the column in `input_data` containing base cost values.
 #' @param year_base A `numeric` or `character`: either a year or the name of a column in `input_data` containing base years.
+#' @param country_base A `character` string: either a country name or the name of a column in `input_data` containing base countries.
 #' @param year_target A `numeric` or `character`: either a year or the name of a column in `input_data` containing target years.
 #' @param country_target A `character`: either a country name or the name of a column in `input_data` containing target countries.
-#' @param cost_base A `character`: the name of the column in `input_data` containing base cost values.
 #' @param cost_target A `character`: the name for the new column that will contain deflated cost values. Defaults to `"cost_target"`.
-#' @param rename_countries A `logical`: whether to standardize country names to ISO3C format. Defaults to `TRUE`.
 #' @param pppex_src A `character`: the source of PPP data, either `"IMF"` or `"OECD"`.
+#' @param rename_countries A `logical`: whether to standardize country names to ISO3C format. Defaults to `TRUE`.
 #' @param use_live_data A `logical`: if `TRUE`, updates internal data if older than one week. Defaults to `TRUE`.
 #' @param force_live_data A `logical`: if `TRUE`, forces an update of internal data regardless of age. Defaults to `FALSE`.
 #'
@@ -43,14 +43,9 @@
 #' )
 #'
 #' # You can also also specify values in the function for year and country
-#' data <- data.frame(
-#'   cost_base = c(100, 200)
-#' )
-#'
 #' # Example 2: Using constant values for base and target
 #' deflated_data <- deflate(
-#'   input_data = data,
-#'   cost_base = "cost_base",
+#'   cost_base = c(100, 200),
 #'   year_base = "2010",
 #'   country_base = "Australia",
 #'   year_target = "2020",
@@ -67,169 +62,169 @@ deflate <- function(
   year_target,
   country_target = "USA",
   cost_target = "cost_target",
-  pppex_src = "IMF",
+  pppex_src = c("IMF", "OECD"),
   rename_countries = TRUE,
   use_live_data = TRUE,
   force_live_data = FALSE
 ) {
-  tryCatch(
-    {
-      # Validate pppex_src
-      if (!pppex_src %in% c("IMF", "OECD")) {
-        stop(sprintf(
-          "`pppex_src` must be either \"IMF\" or \"OECD\". '%s' is not allowed.",
-          pppex_src
-        ))
-      }
+  pppex_src <- match.arg(pppex_src)
 
-      # Validate input_data
-      if (!is.data.frame(input_data) & !tibble::is_tibble(input_data)) {
-        stop("`input_data` must be a data.frame or tibble.")
-      }
+  if (!missing(input_data)) {
+    checkmate::assert_class(input_data, "data.frame")
+  } else {
+    # data are specified with vectors for cost_base -> year_target
+    # combine them into a dataframe so that they can be validated using the same approach
+    # first have to check that they are the right length and class
 
-      # Validate cost_base column
-      if (!(cost_base %in% names(input_data))) {
-        stop(sprintf(
-          "Column '%s' specified in `cost_base` not found in input_data.",
-          cost_base
-        ))
-      }
+    assertthat::assert_that(length(cost_base) > 0)
+    valid_input_length <- c(1, length(cost_base))
 
-      # Validate cost_target name
-      if (cost_target %in% names(input_data)) {
-        warning(sprintf(
-          "Column '%s' already exists in input_data and will be overwritten.",
-          cost_target
-        ))
-      }
+    assertthat::assert_that(length(year_base) %in% valid_input_length)
+    assertthat::assert_that(length(country_base) %in% valid_input_length)
+    assertthat::assert_that(length(year_target) %in% valid_input_length)
+    assertthat::assert_that(length(country_target) %in% valid_input_length)
 
-      # Load reference data
-      tbl <- get_data(pppex_src, use_live_data, force_live_data)
+    input_data <- data.frame(
+      cost_base = cost_base,
+      year_base = year_base,
+      country_base = country_base,
+      year_target = year_target,
+      country_target = country_target
+    )
 
-      # Validate reference data structure
-      required_cols <- c("country", "year", "value_gdpd", "value_pppex")
-      if (!all(required_cols %in% names(tbl))) {
-        stop(
-          "Reference data is missing required columns: country, year, value_gdpd, value_pppex."
-        )
-      }
+    cost_base <- "cost_base"
+    year_base <- "year_base"
+    country_base <- "country_base"
+    year_target <- "year_target"
+    country_target <- "country_target"
+  }
 
-      # Helper: create column if value is not a column name
-      check_and_create_field <- function(df, field_name, user_value) {
-        if (!(user_value %in% names(df))) {
-          df[[field_name]] <- user_value
-          user_value <- field_name
-        }
-        list(df, user_value)
-      }
+  # input_data has been created at this point even if not given as arg
+  # check that columns exist in input_data as they ought to.
+  checkmate::assert_scalar(cost_base)
+  checkmate::assert_character(cost_base)
 
-      # Apply helper to all relevant fields
-      result <- check_and_create_field(input_data, "country_base", country_base)
-      input_data <- result[[1]]
-      country_base <- result[[2]]
+  checkmate::assert_scalar(year_base)
+  checkmate::assert_character(year_base)
 
-      result <- check_and_create_field(input_data, "year_base", year_base)
-      input_data <- result[[1]]
-      year_base <- result[[2]]
+  checkmate::assert_scalar(country_base)
+  checkmate::assert_character(country_base)
 
-      result <- check_and_create_field(input_data, "year_target", year_target)
-      input_data <- result[[1]]
-      year_target <- result[[2]]
+  checkmate::assert_scalar(year_target)
+  checkmate::assert_character(year_target)
 
-      result <- check_and_create_field(
-        input_data,
-        "country_target",
-        country_target
-      )
-      input_data <- result[[1]]
-      country_target <- result[[2]]
-
-      # Helper: ensure numeric conversion
-
-      input_data[[cost_base]] <- can_be_numeric(
-        input_data[[cost_base]],
-        cost_base
-      )
-      input_data[[year_base]] <- can_be_numeric(
-        input_data[[year_base]],
-        year_base
-      )
-      input_data[[year_target]] <- can_be_numeric(
-        input_data[[year_target]],
-        year_target
-      )
-
-      # Clean country names if needed
-      input_data$country_base2 <- if (rename_countries) {
-        country_cleaner(input_data[[country_base]])
-      } else {
-        input_data[[country_base]]
-      }
-      input_data$country_target2 <- if (rename_countries) {
-        country_cleaner(input_data[[country_target]])
-      } else {
-        input_data[[country_target]]
-      }
-
-      check_year_country_combination(
-        input_data,
-        year_base,
-        "country_base2",
-        tbl
-      )
-      check_year_country_combination(
-        input_data,
-        year_target,
-        "country_target2",
-        tbl
-      )
-
-      # Perform deflation
-      output_data <- input_data |>
-        dplyr::left_join(
-          tbl,
-          dplyr::join_by(
-            !!rlang::sym("country_base2") == country,
-            !!rlang::sym(year_base) == year
-          )
-        ) |>
-        dplyr::mutate(
-          deflate_orig = as.numeric(value_gdpd),
-          PPP_orig = as.numeric(value_pppex)
-        ) |>
-        dplyr::select(-value_gdpd, -value_pppex) |>
-        dplyr::left_join(
-          tbl,
-          dplyr::join_by(
-            !!rlang::sym("country_target2") == country,
-            !!rlang::sym(year_target) == year
-          )
-        ) |>
-        dplyr::mutate(
-          deflate_target = as.numeric(value_gdpd),
-          PPP_target = as.numeric(value_pppex)
-        ) |>
-        dplyr::select(-value_gdpd, -value_pppex) |>
-        dplyr::mutate(
-          !!cost_target := (deflate_target * PPP_target) /
-            (deflate_orig * PPP_orig) *
-            .data[[cost_base]]
-        ) |>
-        dplyr::select(
-          -c(
-            "PPP_orig",
-            "deflate_orig",
-            "PPP_target",
-            "deflate_target",
-            "country_base2",
-            "country_target2"
-          )
-        )
-
-      return(output_data)
-    },
-    error = function(e) {
-      stop("deflate() failed: ", e$message)
-    }
+  checkmate::assert_names(
+    names(input_data),
+    must.include = c(cost_base, year_base, country_base, year_target)
   )
+
+  # need to be able to add these as columns to the dataset (after checking that they aren't already columns - throw error if so)
+  # and then continue with the updated arg value being the new column name which contains the new column as that one input value
+
+  checkmate::assert_scalar(cost_target)
+
+  if (cost_target %in% names(input_data)) {
+    warning(sprintf(
+      "Column '%s' already exists in input_data and will be overwritten.",
+      cost_target
+    ))
+    input_data <- dplyr::select(input_data, -!!rlang::sym(cost_target))
+  }
+
+  tbl <- get_data(pppex_src, use_live_data, force_live_data)
+
+  # Validate reference data structure
+  checkmate::assert_names(
+    names(tbl),
+    must.include = c("country", "year", "value_gdpd", "value_pppex")
+  )
+
+  input_data[[cost_base]] <- can_be_numeric(
+    input_data[[cost_base]],
+    cost_base
+  )
+
+  input_data[[year_base]] <- can_be_numeric(
+    input_data[[year_base]],
+    year_base
+  )
+  input_data[[year_target]] <- can_be_numeric(
+    input_data[[year_target]],
+    year_target
+  )
+
+  # Clean country names if needed
+  if (rename_countries) {
+    input_data$country_base2 <- country_cleaner(input_data[[country_base]])
+    input_data$country_target2 <- country_cleaner(input_data[[country_target]])
+  } else {
+    input_data$country_base2 <- input_data[[country_base]]
+    input_data$country_target2 <- input_data[[country_target]]
+  }
+
+  check_year_country_combination(
+    input_data,
+    year_base,
+    "country_base2",
+    tbl
+  )
+
+  check_year_country_combination(
+    input_data,
+    year_target,
+    "country_target2",
+    tbl
+  )
+
+  # Perform deflation
+  input_data |>
+    dplyr::left_join(
+      tbl,
+      dplyr::join_by(
+        !!rlang::sym("country_base2") == country,
+        !!rlang::sym(year_base) == year
+      )
+    ) |>
+    dplyr::mutate(
+      deflate_orig = as.numeric(value_gdpd),
+      PPP_orig = as.numeric(value_pppex)
+    ) |>
+    dplyr::select(-value_gdpd, -value_pppex) |>
+    dplyr::left_join(
+      tbl,
+      dplyr::join_by(
+        !!rlang::sym("country_target2") == country,
+        !!rlang::sym(year_target) == year
+      )
+    ) |>
+    dplyr::mutate(
+      deflate_target = as.numeric(value_gdpd),
+      PPP_target = as.numeric(value_pppex)
+    ) |>
+    dplyr::select(-value_gdpd, -value_pppex) |>
+    dplyr::mutate(
+      !!cost_target := (deflate_target * PPP_target) /
+        (deflate_orig * PPP_orig) *
+        .data[[cost_base]]
+    ) |>
+    dplyr::select(
+      -c(
+        "PPP_orig",
+        "deflate_orig",
+        "PPP_target",
+        "deflate_target",
+        "country_base2",
+        "country_target2"
+      )
+    )
+}
+
+
+# Helper: create column if value is not a column name
+check_and_create_field <- function(df, field_name, user_value) {
+  if (!(user_value %in% names(df))) {
+    df[[field_name]] <- user_value
+    user_value <- field_name
+  }
+  list(df, user_value)
 }
